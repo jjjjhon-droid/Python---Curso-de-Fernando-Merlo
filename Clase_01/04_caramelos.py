dfa54c705bf6da5c6c903788f05956c6dc20d8e4
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 19:45:36 -0300

    4

---
+++ Clase_01/04_caramelos.py
@@ -0,0 +1,7 @@
+# 25 caramelos y 5 nenes. Calcular cuantos caramelos le tocan a cada uno. (//) y cuantos sobran para mi (%)
+
+caramelos = 25
+nenes = 5
+
+print("Cada uno tiene", caramelos // nenes, "caramelos")
+print("Sobran", caramelos % nenes, "caramelos")
