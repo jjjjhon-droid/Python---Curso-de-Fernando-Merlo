a07680d0f2d450dfe14f1933c3f8fb6785548af7
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 02:48:22 -0300

    Eliminado archivos duplicados en la raiz (ya estan en Clase_01)

--- anios_perros.py
+++
@@ -1,4 +0,0 @@
-# Se dice que un año humano equivale a 7 años de perro. Declara una variable edad_humana con un valor y calcula la edad_perruna. Imprime el resultado.
-edad_humana = 5
-edad_perruna = edad_humana * 7
-print(f"Un perro de {edad_humana} años humanos tiene {edad_perruna} años de perro.")
