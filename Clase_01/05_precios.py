ede2e35f15a3ce90f0087ec9eae0307aecf5ed34
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 20:20:58 -0300

    5

---
+++ Clase_01/05_precios.py
@@ -0,0 +1,6 @@
+# Presupuesto de 50 pesos. El producto cuesta 49.99. Crea un programa que diga True si el presupuesto es suficiente (mayor o igual al precio) o False si no.
+
+presupuesto = 50
+producto = 49.99
+
+print(presupuesto >= producto)
