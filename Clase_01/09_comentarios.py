3275fe392e7fcdee2e3e25a24cff3f8fd4894307
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 21:32:03 -0300

    comentarios

---
+++ Clase_01/09_comentarios.py
@@ -0,0 +1,10 @@
+# Entrega este código sucio al alumno y pedile que agregue comentarios explicando qué hace cada línea y cambie las variables x, y, z por nombres descriptivos.
+# Código sucio:
+# x = 100; y = 0.20; z = x - (x*y); print(z)
+
+precio = 100 # precio base
+interes = 0.20 # interes
+precio_final = precio - (precio * interes) # el resultado de la operacion anterior
+print(precio_final) # se imprime el precio final
+
+
