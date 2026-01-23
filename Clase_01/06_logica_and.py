4cbb4cb3dab741f79d01a1a6892aa6fe2bd7254e
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 21:09:47 -0300

    logica

---
+++ Clase_01/06_logica_and.py
@@ -0,0 +1,7 @@
+# Para entrar al sistema, el usuario debe ser "Admin" Y tener la clave "1234". Crear variables para ambos datos y verifica si el acceso es True o False.
+
+usuario = "Admin"
+clave = "1234"
+
+acceso = usuario == "Admin" and clave == "1234"
+print(acceso)
