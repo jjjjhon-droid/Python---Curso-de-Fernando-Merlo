ae4643240a34f8d41006b1601bd0a043848a85c0
Juan Merlo <juanmerlo00@gmail.com>
2026-01-22 21:16:52 -0300

    or

---
+++ Clase_01/07_logica_or.py
@@ -0,0 +1,7 @@
+# Vamos a la playa si "hace sol" O si "es vacaciones". Declara dos variables booleanas y evalúa la condición.
+
+hace_sol = True
+es_vacaciones = False 
+
+ir_a_playa = hace_sol or es_vacaciones
+print(ir_a_playa)
