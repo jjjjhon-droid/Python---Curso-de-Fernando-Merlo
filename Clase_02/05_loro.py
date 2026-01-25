fbb0a5b20d96ca18476c8fc521920686a763fdd1
Juan Merlo <juanmerlo00@gmail.com>
2026-01-25 01:58:08 -0300

    Clase 2

---
+++ Clase_02/05_loro.py
@@ -0,0 +1,8 @@
+# Pide al usuario una palabra y un número. Imprime esa palabra la cantidad de veces que indicó el número.
+
+letra = input("Ingrese una palabra: ")
+numero = int(input("Ingrese un número: "))
+
+for i in range(numero): # aca el numero es la cantidad de veces que se va a repetir
+    print(letra)
+
