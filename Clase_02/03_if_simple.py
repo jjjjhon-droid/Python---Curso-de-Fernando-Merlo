0c621eea32e3d9f5720add9356894cefb77f1f1c
Juan Merlo <juanmerlo00@gmail.com>
2026-01-25 00:49:07 -0300

    Clase 2

---
+++ Clase_02/03_if_simple.py
@@ -0,0 +1,12 @@
+# El precio de una entrada es 50. Pregunta al usuario: "¿Tienes tarjeta VIP? (si/no)".
+#  Si responde "si", aplica un descuento del 50%. Al final imprime el precio a pagar.
+
+entrada = 50
+vip = input("Tienes tarjeta VIP? (si/no)").lower().strip()
+
+if vip == "si":
+    entrada = entrada * 0.5 
+
+# entiendo que aca no es necesario el else, ya que si no se cumple la condicion, el precio sigue siendo el mismo
+
+print(f"El precio a pagar es: {entrada}")
